85d59a2e4d3d5b992de2c1fdb6e59ebb5bbf385b
dcorkhas <dogan.corus@khas.edu.tr>
2024-03-29 14:10:18 +0300

    comments on inventory management

---
+++ Inventory/Source.cpp
@@ -0,0 +1,23 @@
+//Decision variables
+//Reordering point--- number of items
+//Quantity    --- number of items
+// // 
+//Environmental Factors
+//
+//Demand --- Time
+// Interval between demand assuming every customer buys 1
+//       Alternative --- Number of items
+//Delay  --- Time
+//Inventory cost -- Dollars
+//Shortage cost -- Dollars
+//Shelf-life ===We will not implement -- Time
+
+
+//System state variables
+//Inventory level - number of items
+//
+//  
+// 
+// Event Schedule
+// Next demand -- Date
+// Next shipment/order etc. --Date
